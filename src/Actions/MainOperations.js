7024afdc391cffa8579bfea7a9a8829abf436490
massoodbarsa <amir.shakiba1980@@email.com>
2017-09-10 15:33:12 +0200

    new commit

---
+++ src/Actions/MainOperations.js
@@ -0,0 +1,184 @@
+import store from '../store';
+
+
+export function Countable(Digit){
+
+let show=store.state.show
+
+    if (Digit==='3.14') {
+      if(show!=='')
+      return
+    }
+    if (Digit === '.') {
+      store.setState({
+        show: show === '' ? '0' + String(Digit) : show + Digit
+      })
+    } else {
+      store.setState({
+        show: show === '' ? String(Digit) : show + Digit
+      })
+    }
+}
+
+
+
+
+export function Operational(operator) {
+  const {value,show,enterMode,str} = store.state
+  let newValue = [...value]
+
+  if (operator === "CLR") {
+    store.setState({
+      value: [],
+      show: ''
+    })
+    return
+  }
+
+  if (operator === "CLX") {
+    store.setState({
+      show: ''
+    })
+    return
+  }
+
+
+  if(operator==='STO'){
+    let str=newValue[0]
+    store.setState({
+      str
+    })
+  }
+
+
+  if(operator==='RCL'){
+    newValue.push(Number(str))
+    store.setState({
+      value:newValue
+    })
+  }
+
+  if (operator==='R') {
+    const [x,y,z,t]=store.state.value
+    newValue=[t,x,y,z]
+    store.setState({
+      value:newValue
+    })
+  }
+
+
+  if (show !== '') {
+    newValue.push(Number(show))
+    if (newValue.length>4)
+    return
+  }
+
+  if (operator === 'xy') {
+    const [x,y,...rest]=store.state.value
+    newValue=[y,x,...rest]
+    store.setState({
+      value:newValue
+    })
+  }
+
+
+
+
+  if (newValue.length > 1){
+
+      switch (operator) {
+        case '+':
+          newValue[newValue.length - 2] = newValue[newValue.length - 2] + newValue[newValue.length - 1]
+          newValue.pop()
+          break;
+
+        case '-':
+          newValue[newValue.length - 2] = newValue[newValue.length - 2] - newValue[newValue.length - 1]
+          newValue.pop()
+          break;
+
+        case '*':
+          newValue[newValue.length - 2] = newValue[newValue.length - 2] * newValue[newValue.length - 1]
+          newValue.pop()
+          break;
+
+        case '/':
+         if(newValue[newValue.length - 1] !== 0){
+             newValue[newValue.length - 2] = newValue[newValue.length - 2] / newValue[newValue.length - 1]
+          newValue.pop()
+        }
+          break;
+
+        case 'XY':
+          newValue[newValue.length - 2] = Math.pow(newValue[newValue.length - 2], newValue[newValue.length - 1]);
+          newValue.pop()
+          break;
+
+        case 'R':
+
+          break;
+
+        default:
+        break;
+
+      }
+
+    }
+    // store.setState({
+    //   value: newValue,
+    //   show: ''
+    //
+    // })
+
+    if (newValue.length >= 1) {
+      switch (operator) {
+
+
+        case 'SQRT':
+          newValue[newValue.length - 1] = Math.sqrt(newValue[newValue.length - 1])
+          break;
+
+        case 'COS':
+          newValue[newValue.length - 1] = Math.cos(newValue[newValue.length - 1])
+          break;
+
+        case 'SIN':
+          newValue[newValue.length - 1] = Math.sin(newValue[newValue.length - 1])
+          break;
+
+        case 'LN':
+          newValue[newValue.length - 1] = Math.log(newValue[newValue.length - 1])
+          break;
+
+        case 'TAN':
+          newValue[newValue.length - 1] = Math.tan(newValue[newValue.length - 1])
+          break;
+
+        case '1/x':
+          newValue[newValue.length - 1] = 1/newValue[newValue.length - 1]
+          break;
+
+        case 'LOG':
+          newValue[newValue.length - 1] = Math.log(newValue[newValue.length - 1])/Math.LN10
+          break;
+
+        case 'EXP':
+          newValue[newValue.length - 1] = Math.exp(newValue[newValue.length - 1])
+          break;
+
+        case 'CHS':
+          newValue[newValue.length - 1] = -(newValue[newValue.length - 1])
+          break;
+
+        default:
+          break;
+      }
+
+    }
+    store.setState({
+      value: newValue,
+      show: '',
+    })
+
+
+}
---
+++ src/Components/Keyset.js
@@ -0,0 +1,198 @@
+import React from 'react'
+import{CountableButtons,OperationalButtons} from '.'
+import '../Css/Keyset.css'
+
+export default class Keyset extends React.Component{
+
+  render(){
+
+
+    return(
+      <div className ='Keyset'>
+
+        <div className ='keyset_first'>
+               <div className='keyset_first_1'>
+                  <OperationalButtons
+                     label='Xʸ'
+                     value='XY'
+                  />
+                  <OperationalButtons
+                     label='LOG'
+                     value='LOG'
+                  />
+
+                   <OperationalButtons
+                     label='LN'
+                     value='LN'
+                    />
+
+                  <OperationalButtons
+                     label='eˣ'
+                     value='EXP'
+                    />
+                <div className='green'>
+                  <OperationalButtons
+                     label='CLR'
+                     value='CLR'
+                  />
+                 </div>
+                  <OperationalButtons
+                     label='√x'
+                     value='SQRT'
+                  />
+
+                 <div className='pink'>
+                  <OperationalButtons
+                     label='ARC'
+                     value='ARC'
+                  /> </div>
+
+                 <div className='pink'>
+                  <OperationalButtons
+                     label='SIN'
+                     value='SIN'
+                  /> </div>
+                  <div className='pink'>
+                  <OperationalButtons
+                     label='COS'
+                     value='COS'
+                  /></div>
+                  <div className='pink'>
+                  <OperationalButtons
+                     label='TAN'
+                     value='TAN'
+                  /></div>
+
+                  <OperationalButtons
+                     label='¹/x'
+                     value='1/x'
+                  />
+
+                  <OperationalButtons
+                     label='x↔y'
+                     value='xy'
+                  />
+
+                  <OperationalButtons
+                     label='R↓'
+                     value='R'
+                  />
+
+                  <OperationalButtons
+                     label='STO'
+                     value='STO'
+                  />
+
+                  <OperationalButtons
+                     label='RCL'
+                     value='RCL'
+                  />
+               </div>
+               <div className='keyset_first_2' >
+
+                  <OperationalButtons
+                     label='ENTER ↑'
+                     value='ENTER'
+                  />
+
+                  <OperationalButtons
+                     label='CHS'
+                     value='CHS'
+                  />
+
+                  <OperationalButtons
+                     label='EEX'
+                     value=''
+                  />
+
+                  <OperationalButtons
+                     label='CLX'
+                     value='CLX'
+                  />
+            </div>
+          </div>
+
+          <div className ='keyset_second'>
+              <div className='keyset_second_1' >
+                  <OperationalButtons
+                     label='−'
+                     value='-'
+                  />
+                  <OperationalButtons
+                     label='+'
+                     value='+'
+                  />
+                   <OperationalButtons
+                     label='×'
+                     value='*'
+                    />
+                  <OperationalButtons
+                     label='÷'
+                     value='/'
+                    />
+               </div>
+               <div className='keyset_second_2'>
+
+
+                 <CountableButtons
+                    label='7'
+                    value='7'
+                 />
+                 <CountableButtons
+                    label='8'
+                    value='8'
+                 />
+                 <CountableButtons
+                    label='9'
+                    value='9'
+                 />
+
+                 <CountableButtons
+                    label='4'
+                    value='4'
+                 />
+                 <CountableButtons
+                    label='5'
+                    value='5'
+                 />
+                 <CountableButtons
+                    label='6'
+                    value='6'
+                 />
+                  <CountableButtons
+                     label='1'
+                     value='1'
+                  />
+                  <CountableButtons
+                     label='2'
+                     value='2'
+                  />
+                  <CountableButtons
+                     label='3'
+                     value='3'
+                  />
+
+                  <CountableButtons
+                     label='0'
+                     value='0'
+                  />
+                  <CountableButtons
+                     label='.'
+                     value='.'
+                  />
+
+                  <CountableButtons
+                     label='π'
+                     value='3.14'
+                  />
+             </div>
+          </div>
+      </div>
+    )
+  }
+
+
+
+
+
+}
