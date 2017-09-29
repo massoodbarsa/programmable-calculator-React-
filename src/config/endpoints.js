80d6ae55ecf52405a11da5c7be0b73ee82cdafbf
massoodbarsa <amir.shakiba1980@@email.com>
2017-09-29 23:03:25 +0200

    build ready to deploy

--- src/config/endpoints.js
+++
@@ -1,1 +0,0 @@
-const relayApi = 'https://api.graph.cool/relay/v1/cj833f8zr0b6d012005qwn21c'
--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App.js';
-import {Router,browserHistory} from 'react-router'
-import Routes from './routes/index.js'
 
 
 
 
-ReactDOM.render(
-  <Router
-   history={browserHistory}
-   routes={Routes}
-
-  />,
-  // <App />,
-  document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
