b398cf4c840fede8e6886e126575558333207625
massoodbarsa <amir.shakiba1980@@email.com>
2017-10-04 14:01:45 +0200

    server added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App.js';
+import Routes from './routes';
+import {browserHistory}  from 'react-router';
 
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Routes history={browserHistory} />, document.getElementById('root'));
